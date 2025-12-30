a78506aeb44d452d0d02a0b87289e2db7f99d68a
Namopodtaya <Namo@abc.com>
2025-12-30 17:20:54 +0700

    video 51 - 54 20251230  5:24 PM

--- lib/states/edit_product.dart
+++ lib/states/edit_product.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shoppingmall/models/product_model.dart';
 import 'package:shoppingmall/utility/my_constant.dart';
+import 'package:shoppingmall/widgets/show_title.dart';
 
 class EditProduct extends StatefulWidget {
   final ProductModel productModel;
@@ -14,36 +18,79 @@ class EditProduct extends StatefulWidget {
 class _EditProductState extends State<EditProduct> {
   ProductModel? productModel;
   TextEditingController nameController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController detailController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  List<String> pathImages = [];
+  List<File?> files = [];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     productModel = widget.productModel;
-    print('#### name Edit ==>> ${productModel!.name}');
+    // print('#### image form mySQL ==>> ${productModel!.images}');
+    convertStringToArray();
+    nameController.text = productModel!.name;
+    priceController.text = productModel!.price;
+    detailController.text = productModel!.detail;
+  }
+
+  void convertStringToArray() {
+    String string = productModel!.images;
+    // print('string ກ່ອນຕັດ ==>> $string');
+    string = string.substring(1, string.length - 1);
+    // print('string ຫລັງຕັດ ==>> $string');
+    List<String> strings = string.split(',');
+    for (var item in strings) {
+      pathImages.add(item.trim());
+      files.add(null);
+    }
+    print('#### pathImage ==>> $pathImages');
   }
 
   @override
   Widget build(BuildContext context) {
-    double size = MediaQuery.of(context).size.width;
+    // double size = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: MyConstant.whColor,
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => processEdit(),
+            icon: Icon(Icons.edit, size: 32),
+            tooltip: 'Edit Product',
+          ),
+        ],
         backgroundColor: MyConstant.primaryColor,
+        title: Text('Edit product', style: MyConstant().h5NmWCl()),
         iconTheme: IconThemeData(color: MyConstant.whColor),
-        title: Text('Edit product', style: MyConstant().h5BWCl()),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: GestureDetector(
+      body: LayoutBuilder(
+        builder: (context, constraints) => GestureDetector(
           onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-          child: Form(
-            child: SingleChildScrollView(
+          behavior: HitTestBehavior.opaque,
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  buildProductName(size),
-                  buildProductPrice(size),
-                  buildProductDetail(size),
+                  Column(
+                    children: [
+                      buildTitle('General'),
+                      buildProductName(constraints),
+                      buildProductPrice(constraints),
+                      buildProductDetail(constraints),
+                      buildTitle('Image product'),
+                      buildImage(constraints, 0),
+                      buildImage(constraints, 1),
+                      buildImage(constraints, 2),
+                      buildImage(constraints, 3),
+                      buildEditProduct(constraints),
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -53,31 +100,110 @@ class _EditProductState extends State<EditProduct> {
     );
   }
 
-  Row buildProductName(double size) {
+  Container buildEditProduct(BoxConstraints constraints) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 24),
+      width: constraints.maxWidth * 0.95,
+      child: ElevatedButton(
+        style: MyConstant().myButtonSPmr1(),
+        onPressed: () => processEdit(),
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Text('EDIT PRODUCT', style: MyConstant().h4NmWCl()),
+        ),
+      ),
+    );
+  }
+
+  Future<Null> chooseImage(int index, ImageSource source) async {
+    try {
+      var result = await ImagePicker().pickImage(
+        source: source,
+        maxHeight: 240,
+        maxWidth: 240,
+      );
+      setState(() {
+        files[index] = File(result!.path);
+      });
+    } catch (e) {}
+  }
+
+  Container buildImage(BoxConstraints constraints, int index) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: () => chooseImage(index, ImageSource.camera),
+            icon: Icon(
+              Icons.add_a_photo_outlined,
+              color: MyConstant.lightColor,
+              size: 42,
+            ),
+          ),
+          Container(
+            child: files[index] == null
+                ? CachedNetworkImage(
+                    width: constraints.maxWidth * 0.42,
+                    imageUrl:
+                        '${MyConstant.domain}/shoppingmall/${pathImages[index]}',
+                  )
+                : Image.file(files[index]!),
+          ),
+          IconButton(
+            onPressed: () => chooseImage(index, ImageSource.gallery),
+            icon: Icon(
+              Icons.add_photo_alternate_outlined,
+              color: MyConstant.primaryColor,
+              size: 42,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Row buildTitle(String title) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ShowTitle(title: title, textStyle: MyConstant().h4BPmrCl()),
+        ),
+      ],
+    );
+  }
+
+  Row buildProductName(BoxConstraints constraints) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-          margin: EdgeInsets.only(top: 8.0),
-          width: size * 0.91,
+          margin: EdgeInsets.only(top: 16),
+          width: constraints.maxWidth * 0.95,
           child: TextFormField(
             controller: nameController,
             validator: (value) {
               if (value!.isEmpty) {
-                return 'Please enter address';
-              } else {}
+                return 'Please Fill product name in blank';
+              } else {
+                return null;
+              }
             },
-            maxLines: 1,
             decoration: InputDecoration(
-              hintText: 'address',
-              hintStyle: MyConstant().h4NmPmrCl(),
-              prefixIcon: Icon(Icons.home, color: MyConstant.primaryColor),
+              labelText: "Product name",
+              labelStyle: MyConstant().h4NmPmrCl(),
+              prefixIcon: Icon(
+                Icons.production_quantity_limits,
+                color: MyConstant.lightColor,
+              ),
               enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: MyConstant.primaryColor),
+                borderSide: BorderSide(color: MyConstant.lightColor),
                 borderRadius: BorderRadius.circular(9.0),
               ),
               focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: MyConstant.lightColor, width: 2),
+                borderSide: BorderSide(color: MyConstant.darkColor, width: 1),
                 borderRadius: BorderRadius.circular(9.0),
               ),
               errorBorder: OutlineInputBorder(
@@ -91,31 +217,36 @@ class _EditProductState extends State<EditProduct> {
     );
   }
 
-  Row buildProductPrice(double size) {
+  Row buildProductPrice(BoxConstraints constraints) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-          margin: EdgeInsets.only(top: 8.0),
-          width: size * 0.91,
+          margin: EdgeInsets.only(top: 16),
+          width: constraints.maxWidth * 0.95,
           child: TextFormField(
-            controller: nameController,
+            keyboardType: TextInputType.number,
+            controller: priceController,
             validator: (value) {
               if (value!.isEmpty) {
-                return 'Please enter address';
-              } else {}
+                return 'Please Fill product price in blank';
+              } else {
+                return null;
+              }
             },
-            maxLines: 1,
             decoration: InputDecoration(
-              hintText: 'address',
-              hintStyle: MyConstant().h4NmPmrCl(),
-              prefixIcon: Icon(Icons.home, color: MyConstant.primaryColor),
+              labelText: "Product price",
+              labelStyle: MyConstant().h4NmPmrCl(),
+              prefixIcon: Icon(
+                Icons.price_change_outlined,
+                color: MyConstant.lightColor,
+              ),
               enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: MyConstant.primaryColor),
+                borderSide: BorderSide(color: MyConstant.lightColor),
                 borderRadius: BorderRadius.circular(9.0),
               ),
               focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: MyConstant.lightColor, width: 2),
+                borderSide: BorderSide(color: MyConstant.darkColor, width: 1),
                 borderRadius: BorderRadius.circular(9.0),
               ),
               errorBorder: OutlineInputBorder(
@@ -129,34 +260,37 @@ class _EditProductState extends State<EditProduct> {
     );
   }
 
-  Row buildProductDetail(double size) {
+  Row buildProductDetail(BoxConstraints constraints) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-          margin: EdgeInsets.only(top: 8.0),
-          width: size * 0.91,
+          margin: EdgeInsets.only(top: 16),
+          width: constraints.maxWidth * 0.95,
           child: TextFormField(
-            controller: nameController,
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Please enter address';
-              } else {}
-            },
             maxLines: 4,
+            controller: detailController,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill product detail in blank';
+              } else {
+                return null;
+              }
+            },
             decoration: InputDecoration(
-              hintText: 'address',
+              hintText: 'Product detail',
               hintStyle: MyConstant().h4NmPmrCl(),
+              labelStyle: MyConstant().h4NmPmrCl(),
               prefixIcon: Padding(
                 padding: const EdgeInsets.fromLTRB(0, 0, 0, 78),
-                child: Icon(Icons.home, color: MyConstant.primaryColor),
+                child: Icon(Icons.list_alt, color: MyConstant.lightColor),
               ),
               enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: MyConstant.primaryColor),
+                borderSide: BorderSide(color: MyConstant.lightColor),
                 borderRadius: BorderRadius.circular(9.0),
               ),
               focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: MyConstant.lightColor, width: 2),
+                borderSide: BorderSide(color: MyConstant.darkColor, width: 1),
                 borderRadius: BorderRadius.circular(9.0),
               ),
               errorBorder: OutlineInputBorder(
@@ -168,5 +302,14 @@ class _EditProductState extends State<EditProduct> {
         ),
       ],
     );
+  }
+
+  void processEdit() {
+    if (formKey.currentState!.validate()) {
+      String name = nameController.text;
+      String price = priceController.text;
+      String detail = detailController.text;
+      print('#### name = $name, price = $price, detail = $detail');
+    }
   }
 }
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyConstant {
   //General
   static String appName = 'Shopping Mall';
-  static String domain = 'https://f9f29530a47a.ngrok-free.app';
+  static String domain = 'https://81f7e06476ea.ngrok-free.app';
 
   //Route
   static String routeAuthen = '/authen';
